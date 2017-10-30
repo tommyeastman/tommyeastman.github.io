ef63d36d3f89c8dcf01e1bdaca1f089e0b7f19d5
tommyeastman1 <tommyeastman1@gmail.com>
2017-10-30 18:02:59 -0300

    setup for jquery fade

---
+++ app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".name").fadeIn(1000);
+
+  $("#dev").fadeIn(2000);
+
+  $("#data")
+    .delay(1500)
+    .fadeIn(1500);
+
+  $("#ent")
+    .delay(3000)
+    .fadeIn(1500);
+});
